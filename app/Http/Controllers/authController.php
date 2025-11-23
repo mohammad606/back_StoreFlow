81f7567c16a7818c460a533b4f3633bad9d6ecaf
unknown <mohammad123alhalabi123+2@gmail.com>
2025-11-23 04:41:45 +0200

    create login api

--- app/Http/Controllers/authController.php
+++ app/Http/Controllers/authController.php
@@ -9,28 +9,53 @@ use Illuminate\Support\Facades\Hash;
 
 class authController extends Controller
 {
-    public function register(Request $req){
+    public function register(Request $req)
+    {
         $req->validate([
-            'name'=>'required|string|max:255',
-            'email'=>'required|string|email|max:255',
-            'password'=>'required|string|min:6'
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255',
+            'password' => 'required|string|min:6'
         ]);
         $user = User::create([
-            'name'=>$req->name,
-            'email'=>$req->email,
-            'password'=>Hash::make($req->password)
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password)
         ]);
         $token = Auth::login($user);
         return response()->json([
-            'status'=> 'success',
+            'status' => 'success',
             'massage' => 'user created successfully',
-            'user'=> $user,
-            'authorisation'=>[
-                'token'=> $token,
-                'type'=> 'bearer',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer',
             ]
         ]);
     }
 
+    public function login(Request $req)
+    {
+        $req->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string'
+        ]);
+        $credentials = $req->only('email', 'password');
+        $token = Auth::attempt($credentials);
+        if (!$token) {
+            return response()->json([
+                'status' => 'error',
+                'massage' => 'Unauthorized'
+            ], 401);
+        }
+        $user = Auth::user();
+        return response()->json([
+            'status' => 'success',
+            'user' => $user,
+            'authorisation' => [
+                'token' => $token,
+                'type' => 'bearer'
+            ]
+        ]);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/register',[\App\Http\Controllers\authController::class,'register']);
+Route::post('/login',[\App\Http\Controllers\authController::class,'login']);
