a37561f7861406125e1d52052087296a27acdf7b
unknown <mohammad123alhalabi123+2@gmail.com>
2025-11-24 03:11:16 +0200

    add admin role to ueser table and create middleware

---
+++ app/Http/Middleware/CheckAdminRole.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CheckAdminRole
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $user = Auth::user();
+        if (!$user || $user->role !== 'admin') {
+            return response()->json([
+                'status' => false,
+                'message' => 'Unauthorized'
+            ], 403);
+        }
+
+        return $next($request);
+    }
+}
