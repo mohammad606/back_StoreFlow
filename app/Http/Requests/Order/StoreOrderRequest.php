46968e5ba51cd972ea8bd49fd3e63a75446dd1a6
unknown <mohammad123alhalabi123+2@gmail.com>
2025-11-30 18:01:54 +0200

    create requests oreder table

---
+++ app/Http/Requests/Order/StoreOrderRequest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreOrderRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'date' => 'required|date',
+            'sender' => 'required|string|max:50',
+            'noa' => 'required|string|max:50',
+            'customer_id' => 'required|exists:customers,id',
+            'customer_name' => 'required|string|max:50',
+            'items' => 'required|array|min:1',
+            'items.*.product_id' => 'required|exists:store,id',
+            'items.*.quantity' => 'required|integer|min:1',
+        ];
+    }
+}
+
---
+++ app/Http/Requests/Order/UpdateOrderRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Order;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateOrderRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'date' => 'sometimes|date',
+            'sender' => 'sometimes|string|max:50',
+            'noa' => 'sometimes|string|max:50',
+            'customer_id' => 'sometimes|exists:customers,id',
+            'customer_name' => 'sometimes|string|max:50',
+            'deleted_items' => 'sometimes|array',
+            'deleted_items.*' => 'integer|exists:all_input_items,id',
+            'new_items' => 'sometimes|array',
+            'new_items.*.product_id' => 'required|exists:store,id',
+            'new_items.*.quantity' => 'required|integer|min:1',
+            'updated_items' => 'sometimes|array',
+            'updated_items.*.id' => 'required|integer|exists:all_input_items,id',
+            'updated_items.*.product_id' => 'sometimes|exists:store,id',
+            'updated_items.*.quantity' => 'sometimes|integer|min:1',
+        ];
+    }
+}
+
